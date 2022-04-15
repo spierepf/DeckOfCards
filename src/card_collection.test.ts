502536323c07f4451d35cf8caa4d68069ead4e9e
Peter-Frank Spierenburg <peter-frank.spierenburg@navblue.aero>
2022-04-15 09:33:26 -0300

    Added CardCollection::shuffle

--- src/card_collection.test.ts
+++ src/card_collection.test.ts
@@ -45,9 +45,36 @@ test('drawing a card from a singleton CardCollection gets you that card', () => 
     expect(new CardCollection([new Card(Rank.Queen, Suit.Spades)]).draw().equals(new Card(Rank.Queen, Suit.Spades))).toBeTruthy()
 })
 
-
 test('drawing two cards from a doubleton CardCollection gets you both cards in reverse order', () => {
     let deck = new CardCollection([new Card(Rank.Queen, Suit.Spades), new Card(Rank.Two, Suit.Clubs)])
     expect(deck.draw().equals(new Card(Rank.Two, Suit.Clubs))).toBeTruthy()
     expect(deck.draw().equals(new Card(Rank.Queen, Suit.Spades))).toBeTruthy()
+})
+
+test('shuffling a deck of cards does not change the number of cards in the deck', () => {
+    expect(CardCollection.standardDeck().shuffle().size()).toBe(52)
+})
+
+test('shuffling a deck of cards does not change the set of cards in the deck', () => {
+    for(let card1 of CardCollection.standardDeck().shuffle()) {
+        let count = 0
+        for(let card2 of CardCollection.standardDeck()) {
+            if(card1.equals(card2)) {
+                count++
+            }
+        }
+        expect(count).toBe(1)
+    }
+})
+
+test('shuffling two separate decks of cards is unlikely to get you the same ordering', () => {
+    let deck1 = CardCollection.standardDeck().shuffle()
+    let deck2 = CardCollection.standardDeck().shuffle()
+    let count = 0
+    for(let i = deck1.size(); i > 0; --i) {
+        if(deck1.draw().equals(deck2.draw())) {
+            count++
+        }
+    }
+    expect(count).toBeLessThan(52)
 })
--- src/card_collection.ts
+++ src/card_collection.ts
@@ -27,6 +27,24 @@ export class CardCollection implements Iterable<Card> {
         return this.cards.pop()
     }
 
+    // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    shuffle(): CardCollection {
+        let currentIndex = this.cards.length,  randomIndex;
+
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+
+            // Pick a remaining element.
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [this.cards[currentIndex], this.cards[randomIndex]] = [
+                this.cards[randomIndex], this.cards[currentIndex]];
+        }
+        return this
+    }
+
     static deck(ranks: Rank[] = Object.values(Rank), suits: Suit[] = Object.values(Suit), count: int = 1) {
         var cards = []
         for (const rank of ranks) {
