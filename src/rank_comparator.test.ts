458c23d9a9d98ffb7fc631bc29d2775965b076b6
Peter-Frank Spierenburg <peter-frank.spierenburg@navblue.aero>
2022-04-19 10:47:28 -0300

    Added RankComparator

---
+++ src/rank_comparator.test.ts
@@ -0,0 +1,20 @@
+import { RankComparator } from './rank_comparator.ts'
+import { Rank } from './rank.ts'
+
+test('a rank comparator may not be constructed with duplicate ranks', () => {
+    expect(() => {
+        new RankComparator([Rank.Jack, Rank.Jack])
+    }).toThrow(TypeError)
+})
+
+test('a rank comparator constructed with a subset of ranks leaves comparisons involving missing ranks undefined', () => {
+    expect(new RankComparator([Rank.Nine, Rank.Ten, Rank.Jack, Rank.Queen, Rank.King, Rank.Ace]).compare(Rank.Eight, Rank.Nine)).toBeUndefined()
+})
+
+test('two is higher thant ace in king-high ordering', () => {
+    expect(RankComparator.kingHigh().compare(Rank.Two, Rank.Ace)).toBeGreaterThan(0)
+})
+
+test('king is lower thant ace in ace-high ordering', () => {
+    expect(RankComparator.aceHigh().compare(Rank.King, Rank.Ace)).toBeLessThan(0)
+})
